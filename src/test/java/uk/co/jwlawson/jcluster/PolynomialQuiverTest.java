168c6dbe4e45942cb1adf221b1f6148c3a780aad
John Lawson <john@jwlawson.co.uk>
2014-01-28 10:14:32 +0000

    Initial commit

---
+++ src/test/java/uk/co/jwlawson/jcluster/PolynomialQuiverTest.java
@@ -0,0 +1,19 @@
+package uk.co.jwlawson.jcluster;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PolynomialQuiverTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
