c64f38f91e3af7d84f4d04688697cc010c9a82c7
John Lawson <john@jwlawson.co.uk>
2014-02-12 10:30:01 +0000

    Adds demos module

---
+++ jCluster-demos/src/test/java/uk/co/jwlawson/jcluster/demos/AppTest.java
@@ -0,0 +1,21 @@
+package uk.co.jwlawson.jcluster.demos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+  
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
