b558637187540e6566a1429cd7b92da808b67ff4
John Lawson <john@jwlawson.co.uk>
2014-02-16 20:49:14 +0000

    Adds license comment to test

--- jCluster-core/src/test/java/uk/co/jwlawson/jcluster/NewFiniteTaskTest.java
+++ jCluster-core/src/test/java/uk/co/jwlawson/jcluster/NewFiniteTaskTest.java
@@ -1,5 +1,18 @@
 /**
+ * Copyright 2014 John Lawson
  * 
+ * NewFiniteTaskTest.java is part of JCluster.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package uk.co.jwlawson.jcluster;
 
@@ -13,8 +26,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author John
- *
+ * @author John Lawson
+ * 
  */
 public class NewFiniteTaskTest {
 
@@ -23,7 +36,7 @@ public class NewFiniteTaskTest {
 	@Test
 	public void test() {
 		log.debug("Starting test");
-		NewFiniteTask task = new NewFiniteTask(DynkinDiagram.A7.getMatrix());
+		NewFiniteTask task = new NewFiniteTask(DynkinDiagram.A8.getMatrix());
 		ExecutorService exec = Executors.newSingleThreadExecutor();
 
 		Future<Integer> future = exec.submit(task);
