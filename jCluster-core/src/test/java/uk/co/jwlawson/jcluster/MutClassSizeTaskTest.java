8a9294290d0683de21f3681e2a3e7d6f54b4f2fb
John Lawson <john@jwlawson.co.uk>
2014-03-16 11:33:14 +0000

    Renames test to match the class it is testing

--- jCluster-core/src/test/java/uk/co/jwlawson/jcluster/MutClassSizeTaskTest.java
+++ jCluster-core/src/test/java/uk/co/jwlawson/jcluster/MutClassSizeTaskTest.java
@@ -1,7 +1,7 @@
 /**
  * Copyright 2014 John Lawson
  * 
- * NewFiniteTaskTest.java is part of JCluster. Licensed under the Apache License, Version 2.0 (the
+ * MutClassSizeTaskTest.java is part of JCluster. Licensed under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
  * 
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
  * @author John Lawson
  * 
  */
-public class NewFiniteTaskTest {
+public class MutClassSizeTaskTest {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
